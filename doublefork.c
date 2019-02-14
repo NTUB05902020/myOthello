03fd4a126f0bad8d52239a64d9b826683431e070
NTUB05902020 <b05902020@csie.ntu.edu.tw>
2019-02-14 23:16:48 +0800

    modified doublefork.c

--- doublefork.c
+++ doublefork.c
@@ -36,7 +36,7 @@ int main(){
 				printf("Failed to dup2 stderr\n");
 				exit(1);
 			}
-			execlp("./startLearn", "./startLearn", NULL);
+			execlp("./genKifu", "./genKifu", record/CO/0.eval record/CO/1.eval, NULL);
 		}
 	}
 	exit(0);
