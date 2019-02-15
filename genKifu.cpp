04bb8162b919d66e5aaf0c9db3d9caf19ffb9d05
NTUB05902020 <b05902020@csie.ntu.edu.tw>
2019-02-15 09:00:11 +0800

    KifuSizeSmaller

--- genKifu.cpp
+++ genKifu.cpp
@@ -1,19 +1,16 @@
 #include "agent.hpp"
-#include "learn.hpp"
+//#include "learn.hpp"
 #include <thread>
 #include <mutex>
 #include <condition_variable>
 #include <ctime>
 using std::thread;
 using std::mutex;
-using std::max_element;
-using std::min_element;
 using std::unique_lock;
 using std::condition_variable;
 
 #define THREAD_NUM 200
-#define N 10000
-const double ita = 100.0;
+#define N 1000
 
 thread threads[THREAD_NUM];
 int avaID; FILE *fp; time_t currentTime;
@@ -25,13 +22,12 @@ condition_variable cv;
 void playGame(int threadID, const Agent &ag){
 	Board brd;  int bNum, wNum; int result;
 	vector<vector<double>> tmp;
-	while(!brd.getLegalMoves().empty()){
+	do{
 		vector<double> tmp2 = brd.getBoardVec();
+		brd.getNum(bNum, wNum); tmp2.push_back(bNum-wNum);
 		tmp2.push_back(1.0); tmp.push_back(tmp2);
 		brd = brd.getNextBoard(ag.getMove(brd));
-	}
-	vector<double> tmp2 = brd.getBoardVec(); tmp2.push_back(1.0);
-	tmp.push_back(tmp2); brd.getNum(bNum, wNum);
+	}while(!brd.getLegalMoves().empty());
 	if(bNum > wNum) result = 1;
 	else result = (wNum > bNum)? -1:0;
 	fpLock.lock();
@@ -39,22 +35,22 @@ void playGame(int threadID, const Agent &ag){
 		for(int j=0;j<tmp[i].size();++j){
 			fprintf(fp, "%d ", (int)tmp[i][j]);
 		}
-		fprintf(fp, "%d\n", result);
+		fprintf(fp, "%d\n", result); fflush(fp);
 	}
 	fpLock.unlock(); avaIDLock.lock(); avaID = threadID; cv.notify_all();
 }
-
+/*
 void printVec(const vector<double> &v){
 	printf("vector:  ");
 	for(auto i=v.begin();i!=v.end();++i) printf("%lf ", *i);
 	printf("\n");
 }
+*/
 
 int main(int argc, char **argv){
 	if(argc != 3){ printf("format: %s [evalFile] [writeTo]\n", argv[0]); exit(1);}
 	//play game
-	currentTime = time(NULL);
-	printf("%s\n", ctime(&currentTime));
+	currentTime = time(NULL); printf("%s\n", ctime(&currentTime)); fflush(stdout);
 	Agent ag(CORNER, argv[1], argv[1], 5, true, 0.5);
 	if((fp = fopen(argv[2], "w")) == NULL){ printf("Failed to open %s\n", argv[2]); exit(1);}
 	int called = (N < THREAD_NUM)? N:THREAD_NUM, done = 0;
@@ -67,7 +63,6 @@ int main(int argc, char **argv){
 		cv.wait(cvULM); threads[avaID].join();
 		avaIDLock.unlock(); ++done;
 	}
-	currentTime = time(NULL);
-	printf("%s\n", ctime(&currentTime));
-	fflush(fp); fclose(fp); exit(0);
+	currentTime = time(NULL); printf("%s\n", ctime(&currentTime)); fflush(stdout);
+	fclose(fp); exit(0);
 }
