21d7b3704d01544cea55cf20744844ccbc1cb1c6
NTUB05902020 <b05902020@linux4.csie.ntu.edu.tw>
2019-02-17 12:42:51 +0800

    ita smaller

--- learn.cpp
+++ learn.cpp
@@ -19,7 +19,7 @@ mutex nowMutex, einMutex, graMutex;
 time_t currentTime;
 FILE *fp;
 int N = 0, now = 0;
-double ita=1000000.0, lr=0.0, log_2 = log(2), ein;
+double ita=1.0, lr=0.0, log_2 = log(2), ein;
 int order[D] = {0,1,2,5,6,7,8,9,10,13,14,15,16,17,18,21,22,23,40,41,42,45,46,47,48,49,50,53,54,55,56,57,58,61,62,63,64,65};
 vector<array<double, D>> X;
 vector<int> Y;
